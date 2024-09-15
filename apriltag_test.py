0242b46e26f2ad54e58101fb4bc606f3c7db1458
Jonathan Li <jonathanli668899@gmail.com>
2024-09-14 20:49:03 -0400

    AprilTagPupil w/ networktables for possibly actually taking to the roborio?

--- apriltag_test.py
+++ apriltag_test.py
@@ -1,6 +1,10 @@
 import cv2
 from pupil_apriltags import Detector
 import numpy as np
+from networktables import NetworkTables;
+
+NetworkTables.initialize(server='roborio-3205-frc.local') #roborio address? online ppl say its team number or somthing
+sd = NetworkTables.getTable('SmartDashboard')
 
 cap = cv2.VideoCapture(0)
 
@@ -22,7 +26,6 @@ while True:
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
     #if want pose hafta have camera params fx, fy, cx, cy (focal lengths, optical center???)
     #how do u use checkerboard to get these tho?
     tags = at_detector.detect(gray, estimate_tag_pose=False, camera_params= None, tag_size=0.1)
@@ -34,9 +37,12 @@ while True:
         ptC = (int(ptC[0]), int(ptC[1]))
         ptD = (int(ptD[0]), int(ptD[1]))
         
-        #BGR NOT RGB
+        # BGR NOT RGB
         cv2.polylines(frame, [np.array([ptA, ptB, ptC, ptD], dtype=np.int32)], isClosed=True, color=(0, 255, 0), thickness=2)
         cv2.putText(frame, str(tag.tag_id), ptA, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+        sd.putNumber('tagID', tag.tag_id)
+        sd.putNumberArray('tagPose', tag.pose_t.flatten())
 
     cv2.imshow("frame", frame)
     
@@ -45,4 +51,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-#print("hi")
