973d30032b4018c83b7d2ccec745c449ecccaf4f
AlextheChild <alexjun2008@gmail.com>
2024-09-19 16:24:48 -0400

    hi

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -11,5 +11,7 @@ public final class Main {
 
     public static void main(String... args) {
         RobotBase.startRobot(Robot::new);
+        System.out.println("hi");
+        System.out.println("hello");
     }
 }
